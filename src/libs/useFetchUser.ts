36f16e60b24c9c9cbaddde9c5e646ce181f33e43
Barış DEMİRCİ <demirci.baris38@gmail.com>
2020-12-06 13:23:16 +0300

    🖊️ Delete and report paste

--- src/libs/useFetchUser.ts
+++ src/libs/useFetchUser.ts
@@ -29,32 +29,6 @@ export async function fetchUser(access_token?: string): Promise<IUser | null> {
 	} else return null;
 }
 
-/*
-export function useFetchUser(strict = true): { user: IUser; loading: boolean; setUser: Dispatch<SetStateAction<IUser>>; setLoading: Dispatch<SetStateAction<boolean>>; } {
-	const [loading, setLoading] = useState(() => typeof window !== "undefined");
-	const [user, setUser] = useState<IUser>(null);
-	const [cookies, , removeCookie] = useCookies();
-	const access_token = cookies["access_token"];
-
-	useEffect(() => {
-		if (!loading && user) return;
-		setLoading(true);
-		if (access_token) {
-			fetchUser(access_token).then((userData) => {
-				if (!userData) {
-					removeCookie("access_token");
-					if (strict) return (window.location.href = "/login");
-				} else {
-					setUser({ ...userData, access_token });
-					setLoading(false);
-				}
-			});
-		} else if (strict) window.location.href = "/login";
-	}, []);
-
-	return { user, loading, setUser, setLoading };
-}
-*/
 export function useFetchUser(
 	strict = true,
 ): {
@@ -80,7 +54,7 @@ export function useFetchUser(
 					window.location.href = "/login";
 					return;
 				}
-				setUser(user);
+				setUser({ ...user, access_token });
 				setLoading(false);
 			}
 		});
--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -7,6 +7,9 @@ import { useFetchUser } from "@libs/useFetchUser";
 import { useRouter } from "next/router";
 import CONFIG from "src/config";
 import Layout from "@components/Layout/index";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import BarLoader from "@components/BarLoader";
 
 export interface IFilePage {
 	error?: boolean;
@@ -23,10 +26,56 @@ export interface IFilePage {
 
 const FilePage: NextPage<IFilePage> = ({ error, pasteData }) => {
 	const router = useRouter();
-	const { user, loading } = useFetchUser(false);
+	const { user, loading: userLoading } = useFetchUser(false);
+	const [loading, setLoading] = useState(false);
+	const [canEdit, setCanEdit] = useState(false);
+	const [canReport, setCanReport] = useState(false);
+
+	useEffect(() => {
+		if (!error && user && pasteData) {
+			setCanReport(pasteData.owner !== user.user.id && !pasteData.is_reported);
+			setCanEdit(pasteData.owner === user.user.id && !pasteData.is_reported);
+		}
+	});
+
+	const handleReport = async () => {
+		if (loading) return;
+		if (!canReport) return toast.error("❌ You can't report this paste.");
+		setLoading(true);
+		const res = await fetch(`${CONFIG.API_URL}/paste/report?id=${router.query.id}`, {
+			headers: {
+				"Authorization": user.access_token
+			}
+		});
+		const body = await res.json();
+		setLoading(false);
+		if (!body.data) return toast.error("❌ Please try again later."); 
+		toast.success("✔️ Thank you for reporting!");
+		router.push("/profile");
+	}
+
+	const handleDelete = async () => {
+		if (loading) return;
+		if (!canEdit) return toast.error("❌ You can't delete this paste.");
+		setLoading(true);
+		const res = await fetch(`${CONFIG.API_URL}/paste`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": user.access_token
+			},
+			body: JSON.stringify({
+				id: router.query.id
+			})
+		});
+		const body = await res.json();
+		setLoading(false);
+		if (!body.data) return toast.error("❌ Please try again later.");
+		router.push("/profile");
+	}
 
 	return (
-		<Layout user={user} loading={loading}>
+		<Layout user={user} loading={userLoading}>
 			<NextSeo title="Explore Paste" />
 			<div>
 				<FileHeader
@@ -45,20 +94,51 @@ const FilePage: NextPage<IFilePage> = ({ error, pasteData }) => {
 					}
 					id={router.query.id as string}
 				/>
-				<div className="container max-w-screen-xl px-2 mx-auto lg:px-0">
-					<div className="px-5 py-5 -mt-5 text-sm text-black bg-white rounded-lg shadow ">
-						<SyntaxHighlighter
-							style={tomorrowNight}
-							showLineNumbers={true}
-							wrapLongLines={true}
-						>
-							{pasteData && pasteData.is_reported
-								? "This paste is reported and unavailable for now"
-								: error
-								? "There must have been some very cool and meaningful things written here... Anyways, why don't try something else?"
-								: pasteData.paste}
-						</SyntaxHighlighter>
-					</div>
+				<div className="container max-w-screen-xl px-5 mx-auto -mt-10 lg:px-0">
+					<ul className="grid w-full grid-cols-1 px-5 py-5 bg-white rounded-lg lg:grid-cols-3 -gap-4">
+						<li className="flex flex-col w-full col-span-3 px-5 py-5 space-y-2 bg-transparent rounded-lg">
+							<SyntaxHighlighter
+								style={tomorrowNight}
+								showLineNumbers={true}
+								wrapLongLines={true}
+							>
+								{pasteData && pasteData.is_reported
+									? "This paste is reported and unavailable for now"
+									: error
+									? "There must have been some very cool and meaningful things written here... Anyways, why don't try something else?"
+									: pasteData.paste}
+							</SyntaxHighlighter>
+						</li>
+						<li className="flex flex-col w-full col-span-3 px-5 py-5 space-y-2 bg-transparent rounded-lg lg:col-span-1">
+							<button
+								className={`w-full col-span-3 px-3 py-3 text-sm font-medium text-white transition duration-150 bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none ${
+									!canEdit || loading ? "cursor-not-allowed" : "cursor-pointer"
+								}`}
+							>
+								{loading ? <BarLoader /> : "Edit"}
+							</button>
+						</li>
+						<li className="flex flex-col w-full col-span-3 px-5 py-5 space-y-2 bg-transparent rounded-lg lg:col-span-1">
+							<button
+								onClick={handleDelete}
+								className={`w-full col-span-3 px-3 py-3 text-sm font-medium text-white transition duration-150 bg-pink-500 rounded-lg hover:bg-pink-600 focus:outline-none ${
+									!canEdit || loading ? "cursor-not-allowed" : "cursor-pointer"
+								}`}
+							>
+								{loading ? <BarLoader /> : "Delete"}
+							</button>
+						</li>
+						<li className="flex flex-col w-full col-span-3 px-5 py-5 space-y-2 bg-transparent rounded-lg lg:col-span-1">
+							<button
+								onClick={handleReport}
+								className={`w-full col-span-3 px-3 py-3 text-sm font-medium text-white transition duration-150 bg-yellow-500 rounded-lg hover:bg-yellow-600 focus:outline-none ${
+									!canReport || loading ? "cursor-not-allowed" : "cursor-pointer"
+								}`}
+							>
+								{loading ? <BarLoader /> : "Report"}
+							</button>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</Layout>
